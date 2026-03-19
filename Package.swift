7d37c086461c1bcb6e096b419ffdce9bfbb45d05
YandexMapKit <robot-mapkit-ci@yandex-team.ru>
2026-03-19 17:38:07 +0300

    Publish swift package YandexMapsMobile version 4.33.1

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobileNavikit",
-            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.33.0-navikit.framework.zip",
-            checksum: "af381905d70c78a9667f01aa9e5d52c338ff44456ee514ab01469b0aec988c85"
+            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.33.1-navikit.framework.zip",
+            checksum: "583b3a951a60781a6883d1c795de92a66d5b9da57ff9eb5f58b0aa594fa831bb"
         ),
         .target(
             name: "YandexMapsMobileNavikitResources",
